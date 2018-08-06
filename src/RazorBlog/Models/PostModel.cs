a5182dc7d884a64a0467c14ebb8db0cdc786a7d8
Håkan Edling <hakan@tidyui.com>
2018-08-06 22:14:05 +0200

    Moved PostModel to Models

--- src/RazorBlog/Models/PostModel.cs
+++ src/RazorBlog/Models/PostModel.cs
@@ -14,7 +14,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorBlog.Services;
 
-namespace RazorBlog.PageModels
+namespace RazorBlog.Models
 {
     /// <summary>
     /// Page model for a single post.
